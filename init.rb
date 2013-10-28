fe97bd402b9a374fc2ea6d06dc02ed42cde0814f
edgar <edgar@mottimotti.com>
2013-10-28 12:53:36 +0200

    overwriting the view

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :issue_link_with_original do
   author 'EdgarK'
   description 'This is a plugin for Redmine that adds chackbox on new copied issue saying "link with original".'
   version '0.0.1'
-  url 'https://github.com/EdgarK/issue_counter'
+  url 'https://github.com/EdgarK/issue_link_with_original'
   author_url 'http://nopage'
 end
