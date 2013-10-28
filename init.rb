ba329eec743073c25b603e34f8ea2a6ff7c7d593
edgar <edgar@mottimotti.com>
2013-10-28 12:20:50 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_link_with_original do
+  name 'Issue Link With Original plugin'
+  author 'EdgarK'
+  description 'This is a plugin for Redmine that adds chackbox on new copied issue saying "link with original".'
+  version '0.0.1'
+  url 'https://github.com/EdgarK/issue_counter'
+  author_url 'http://nopage'
+end
